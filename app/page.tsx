a9e46a6995183fe52dc779ceb05cd135b222d562
tqspham <tqs.pham@gmail.com>
2026-04-28 20:20:17 +0200

    Create app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import TodoApp from "@/components/TodoApp";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-4">
+      <TodoApp />
+    </main>
+  );
+}
