f844055e7a568397e21dbdc86f233874636a5d73
tqspham <tqs.pham@gmail.com>
2026-04-28 23:11:33 +0200

    Update app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-gray-50">{children}</body>
+      <body className="bg-blue-50">{children}</body>
     </html>
   );
 }
